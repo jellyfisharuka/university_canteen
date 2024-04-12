81075f7d4d08d9eb89eab76ea3b64f8d5c511df8
jellyfisharuka <arukeulen@gmail.com>
2024-04-12 20:32:19 +0500

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	storage "final_project/internal"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	config := &storage.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		Password: os.Getenv("DB_PASS"),
+		User:     os.Getenv("DB_USER"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+		DBName:   os.Getenv("DB_NAME"),
+	}
+
+	 db, err := storage.NewConnection(config)
+
+	if err != nil {
+		log.Fatal("could not load the database", db)
+	}
+}
