1ef1285f4a3ecf461b0bff55c9368b9c6b4b566c
Mukagali <mukagaly.omirzak2003@gmail.com>
2024-05-01 18:00:22 +0500

    swagger

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"final_project/initializers"
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"net/http"
 )
 
 func init() {
@@ -9,6 +13,26 @@ func init() {
 	initializers.ConnectDb()
 }
 
+// @title						Gallery service
+//
+//	@version					1.0
+//	@description				Gallery service
+//	@termsOfService				http://swagger.io/terms/
+//	@license.name				Apache 2.0
+//	@license.url				http://www.apache.org/licenses/LICENSE-2.0.html
+//	@host						localhost:8082
+//	@securityDefinitions.apikey	BearerAuth
+//	@type						apiKey
+//	@name						Authorization
+//	@in							header
+//	@schemes
 func main() {
+	r := gin.Default()
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
+	r.Run(":8080")
+}
+
+func PingExample(c *gin.Context) {
+	c.JSON(http.StatusOK, "pong")
 }
