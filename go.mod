42a604aee5e6480b7c2b0557343325e52ac12726
Nananina2004 <nurayna.medresova@bk.ru>
2024-05-01 21:10:40 +0500

    menu endpoints added

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module final_project
 
-go 1.22.0
+go 1.23
 
 require github.com/go-sql-driver/mysql v1.8.1 // indirect
 
@@ -29,6 +29,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.4.3 // indirect
--- internal/router/router.go
+++ internal/router/router.go
@@ -62,6 +62,71 @@ func SetupRouter() *gin.Engine {
 
 		c.JSON(http.StatusCreated, gin.H{"message": "User signed up successfully"})
 	})
+	menuRoutes := router.Group("/menu", utils.AuthMiddleware())
+	{
+		menuRoutes.POST("/", func(c *gin.Context) {
+			role, _ := c.Get("role")
+			if role == "admin" {
+			var menuItem models.Menu
+			if err := c.BindJSON(&menuItem); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+				return
+			}
+			if err := initializers.DB.Create(&menuItem).Error; err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add menu item"})
+				return
+			}
+			c.JSON(http.StatusCreated, gin.H{"message": "Menu item added successfully", "menuItemId": menuItem.ID})
+		} else if role == "client"{
+			c.JSON(http.StatusForbidden, gin.H{"error": "Insufficient permissions"})
+        	return
+		}
+	})
+
+	menuRoutes.PUT("/:itemId", func(c *gin.Context) {
+		role, _ := c.Get("role")
+		if role != "admin" {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Insufficient permissions"})
+			return
+		}
+		var menuItem models.Menu
+		itemId := c.Param("itemId")
+		if err := initializers.DB.First(&menuItem, itemId).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Menu item not found"})
+			return
+		}
+		if err := c.BindJSON(&menuItem); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+			return
+		}
+		initializers.DB.Save(&menuItem)
+		c.JSON(http.StatusOK, gin.H{"message": "Menu item updated successfully"})
+	})
+
+	menuRoutes.DELETE("/:itemId", func(c *gin.Context) {
+		role, _ := c.Get("role")
+		if role != "admin" {
+			c.JSON(http.StatusForbidden, gin.H{"error": "Insufficient permissions"})
+			return
+		}
+		itemId := c.Param("itemId")
+		if err := initializers.DB.Delete(&models.Menu{}, itemId).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete menu item"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message": "Menu item deleted successfully"})
+	})
+
+	menuRoutes.GET("/", func(c *gin.Context) {
+		var menuItems []models.Menu
+		if err := initializers.DB.Find(&menuItems).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve menu items"})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"menuItems": menuItems})
+	})
+}
+
 
 	return router
 }
