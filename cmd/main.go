73214ab6c98eb936f3d69e649d53f155925eda38
Mukagali <mukagaly.omirzak2003@gmail.com>
2024-05-01 18:19:23 +0500

    add db changes

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"final_project/initializers"
-	"github.com/gin-gonic/gin"
 	"final_project/internal/router"
 )
 
@@ -11,20 +10,7 @@ func init() {
 	initializers.ConnectDb()
 }
 
-// @title						Gallery service
-//
-//	@version					1.0
-//	@description				Gallery service
-//	@termsOfService				http://swagger.io/terms/
-//	@license.name				Apache 2.0
-//	@license.url				http://www.apache.org/licenses/LICENSE-2.0.html
-//	@host						localhost:8082
-//	@securityDefinitions.apikey	BearerAuth
-//	@type						apiKey
-//	@name						Authorization
-//	@in							header
-//	@schemes
 func main() {
 	router := router.SetupRouter()
-
+	router.Run(":8000")
 }
