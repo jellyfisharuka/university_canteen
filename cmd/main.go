0080e59db373a30a0f5e954adcedab79b0f12e22
jellyfisharuka <arukeulen@gmail.com>
2024-06-02 22:18:19 +0500

    :memo: just checking

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"final_project/initializers"
 	"final_project/internal/router"
+	"fmt"
 	"log"
+
+	"github.com/jomei/notionapi"
 )
 
 func init() {
@@ -12,10 +16,17 @@ func init() {
 }
 
 func main() {
-	
+	client := notionapi.NewClient("secret_6Z366pdbdqmV5H9RP5K2ksjoA1W44E6pUR6A5YSnguq")
+	page, err := client.Page.Get(context.Background(), "your_page_id")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Page ID:", page.ID)
+    //fmt.Println("Page Title:", page.Title)
+    fmt.Println("Page URL:", page.URL)
 	router := router.SetupRouter()
-	err := router.Run(":8092")
-if err != nil {
+	error := router.Run(":8092")
+if error != nil {
     log.Fatal(err)
 }
 
--- internal/router/router.go
+++ internal/router/router.go
@@ -14,6 +14,7 @@ import (
 	"time"
 )
 
+
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
 	setupPublicEndpoints(router)
